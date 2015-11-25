3f154e20c9437782cd32b5abe5c07e6135a76a99
John Seymour <jseymour@zerofox.com>
2015-11-25 15:27:24 -0500

    removed newlines

--- hexdump_ngrams/ngram_example.py
+++ hexdump_ngrams/ngram_example.py
@@ -1,18 +1,19 @@
 import nltk
 from nltk import bigrams, trigrams
 from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.util import ngrams
 
 with open("hexdump",'r') as file:
     bytes = file.read()
     byte_tokens = nltk.regexp_tokenize(bytes, pattern='\w\w')
 
-byte_bigrams = bigrams(byte_tokens)
-byte_trigrams = trigrams(byte_tokens)
+byte_fourgrams = ngrams(byte_tokens, 4)
 
-vectorizer = TfidfVectorizer(min_df=2)
-X = vectorizer.fit_transform(''.join(byte_bigram) for byte_bigram in byte_bigrams)
+vectorizer = TfidfVectorizer(min_df=3)
+X = vectorizer.fit_transform(''.join(byte_fourgram) for byte_fourgram in byte_fourgrams)
 idf = vectorizer.idf_
 
 tf_idfs = dict(zip(vectorizer.get_feature_names(), idf))
 for bytes in tf_idfs:
-    print bytes, tf_idfs[bytes]
+    print bytes, bytes.decode("hex").replace("\n"," "), tf_idfs[bytes]
+
